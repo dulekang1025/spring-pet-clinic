b1400744ec3f9b30b25c7940effbbecab01caa29
Lekang <lekangdu@gmail.com>
2020-06-19 21:07:08 -0400

    Refactor service interface to a common interface. Closes #10.

--- pet-clinic-data/src/main/java/lekang/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/lekang/petclinic/services/VetService.java
@@ -4,12 +4,6 @@ import lekang.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
