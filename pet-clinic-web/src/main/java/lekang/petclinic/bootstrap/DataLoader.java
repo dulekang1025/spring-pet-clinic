5edc233b40499ab6ac43efe40ebc5698283c6d40
Lekang <lekangdu@gmail.com>
2020-06-24 22:30:30 -0400

    Add pettypes in bootsttap.  Closes #32.

--- pet-clinic-web/src/main/java/lekang/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/lekang/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package lekang.petclinic.bootstrap;
 
 import lekang.petclinic.model.Owner;
+import lekang.petclinic.model.PetType;
 import lekang.petclinic.model.Vet;
 import lekang.petclinic.services.OwnerService;
+import lekang.petclinic.services.PetTypeService;
 import lekang.petclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Alice");
         owner1.setSecondName("Swan");
