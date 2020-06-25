0dc15e0aace30720c91c6f965ea413d359a794a7
Lekang <lekangdu@gmail.com>
2020-06-24 21:42:30 -0400

    Created vet speciality entity, associate to vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/lekang/petclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package lekang.petclinic.model;
+
+public class Speciality extends BaseEnity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
