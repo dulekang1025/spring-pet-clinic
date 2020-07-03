4051555f6adba64273c454b3991ec9d0627740b1
Lekang <lekangdu@gmail.com>
2020-07-03 17:02:43 -0400

    Convert vets to jpa entities. Closes #37.

--- pet-clinic-data/src/main/java/lekang/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/lekang/petclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package lekang.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEnity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
