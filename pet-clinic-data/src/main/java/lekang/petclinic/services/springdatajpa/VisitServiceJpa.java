b75ced77e8da49d9d5decda8e84bf697f35f3a66
Lekang <lekangdu@gmail.com>
2020-07-04 10:38:58 -0400

    Created Spring Data Jpa service for Visit. Closes #47.

---
+++ pet-clinic-data/src/main/java/lekang/petclinic/services/springdatajpa/VisitServiceJpa.java
@@ -0,0 +1,49 @@
+package lekang.petclinic.services.springdatajpa;
+
+import lekang.petclinic.model.Visit;
+import lekang.petclinic.repositories.VisitRepository;
+import lekang.petclinic.services.CrudService;
+import lekang.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJpa")
+public class VisitServiceJpa implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJpa(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
