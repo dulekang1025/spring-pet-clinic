7f0af7c94574cf339d2966b5970fb686ac3072a0
Lekang <lekangdu@gmail.com>
2020-07-03 22:28:06 -0400

    Created spring data jpa service for vets. Closes #41.

--- pet-clinic-data/src/main/java/lekang/petclinic/services/springdatajpa/OwnerServiceJpa.java
+++ pet-clinic-data/src/main/java/lekang/petclinic/services/springdatajpa/OwnerServiceJpa.java
@@ -5,19 +5,22 @@ import lekang.petclinic.repositories.OwnerRepository;
 import lekang.petclinic.repositories.PetRepository;
 import lekang.petclinic.repositories.PetTypeRepository;
 import lekang.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-
-public class OwnerJpaService implements OwnerService {
+@Service
+@Profile("SpringDataJpa")
+public class OwnerServiceJpa implements OwnerService {
 
     private final OwnerRepository ownerRepository;
     private final PetRepository petRepository;
     private final PetTypeRepository petTypeRepository;
 
-    public OwnerJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+    public OwnerServiceJpa(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
         this.ownerRepository = ownerRepository;
         this.petRepository = petRepository;
         this.petTypeRepository = petTypeRepository;
---
+++ pet-clinic-data/src/main/java/lekang/petclinic/services/springdatajpa/PetServiceJpa.java
@@ -0,0 +1,54 @@
+package lekang.petclinic.services.springdatajpa;
+
+import lekang.petclinic.model.Pet;
+import lekang.petclinic.repositories.PetRepository;
+import lekang.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJpa")
+public class PetServiceJpa implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> set = new HashSet<>();
+        petRepository.findAll().forEach(set::add);
+        return null;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        Optional<Pet> optionalPet = petRepository.findById(aLong);
+        if(optionalPet.isPresent()){
+            return optionalPet.get();
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
